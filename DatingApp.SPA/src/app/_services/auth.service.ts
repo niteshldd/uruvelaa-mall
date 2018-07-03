f5894d47b86a92d4da270e3f3153107041ae9121
Nitesh.ldd <nitesh.ldd@gmail.com>
2018-07-03 07:26:16 +0530

    Uruvelaa in production

---
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,79 @@
+import { environment } from './../../environments/environment';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
+import { User } from '../_models/user';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class AuthService {
+  baseUrl = environment.apiUrl;
+  userToken: any;
+  decodedToken: any;
+  currentUser: User;
+  jwtHelper: JwtHelper = new JwtHelper();
+  private photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
+
+  constructor(private http: Http) {}
+
+  changeMemberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl);
+  }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'auth/login', model, this.requestOptions()).map((response: Response) => {
+      const user = response.json();
+      if (user) {
+        localStorage.setItem('token', user.tokenString);
+        localStorage.setItem('user', JSON.stringify(user.user));
+        this.decodedToken = this.jwtHelper.decodeToken(user.tokenString);
+        this.currentUser = user.user;
+        this.userToken = user.tokenString;
+        if (this.currentUser.photoUrl !== null) {
+          this.changeMemberPhoto(this.currentUser.photoUrl);
+        } else {
+          this.changeMemberPhoto('../../assets/user.png');
+        }
+
+      }
+    }).catch(this.handleError);
+  }
+
+  register(user: User) {
+    return this.http.post(this.baseUrl + 'auth/register', user, this.requestOptions()).catch(this.handleError);
+  }
+
+  loggedIn() {
+    return tokenNotExpired('token');
+  }
+
+  private requestOptions() {
+    const headers = new Headers({'Content-type': 'application/json'});
+    return new RequestOptions({headers: headers});
+  }
+
+  private handleError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+    if (applicationError) {
+      return Observable.throw(applicationError);
+    }
+    const serverError = error.json();
+    let modelStateErrors = '';
+    if (serverError) {
+      for (const key in serverError) {
+        if (serverError[key]) {
+          modelStateErrors += serverError[key] + '\n';
+        }
+      }
+    }
+    return Observable.throw(
+      modelStateErrors || 'Server error'
+    );
+  }
+
+}
